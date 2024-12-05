c5326cf54ac55dd08237627008cc76486bea8149
fuadhasen <fuadhas6634@gmail.com>
2024-12-05 23:37:18 +0530

    redis client update

--- utils/redis.js
+++ utils/redis.js
@@ -1,25 +1,37 @@
 import { createClient } from 'redis'
+const { promisify } = require('util')
 
 
 class RedisClient {
   constructor() {
     this.client  = createClient();
-    client.on('error', (err) => {
+    this.client.on('error', (err) => {
         console.log(err)
     });
   }
 
-    // ES6 way of creating function
+  // ES6 way of creating function
   isAlive() {
-    return new Promise((resolve, reject) => {
-      this.client.on('error', () => {
-        reject(false);
-      })
-
-      this.client.on('connect', () => {
-        resolve(true);
-      })
-    })
-
+    return this.client.connected;
   }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client)
+    const reply = await getAsync(key)
+    return reply;
+  }
+
+  async set(key, value, duration) {
+    const setexAsync = promisify(this.client.setex).bind(this.client)
+    await setexAsync(key, duration, value)
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client)
+    await delAsync(key)
+  }
+
 }
+
+const redisClient = new RedisClient()
+module.exports = redisClient
