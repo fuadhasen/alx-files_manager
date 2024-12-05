e9b4586fb5f5891fcc16eaf226ba577b6ccbdf50
fuadhasen <fuadhas6634@gmail.com>
2024-12-05 14:28:15 +0530

    redis client connection created '

--- utils/redis.js
+++ utils/redis.js
@@ -2,10 +2,24 @@ import { createClient } from 'redis'
 
 
 class RedisClient {
-    constructor(name) {
-        const client  = createClient();
+  constructor() {
+    this.client  = createClient();
+    client.on('error', (err) => {
+        console.log(err)
+    });
+  }
 
+    // ES6 way of creating function
+  isAlive() {
+    return new Promise((resolve, reject) => {
+      this.client.on('error', () => {
+        reject(false);
+      })
 
-    }
+      this.client.on('connect', () => {
+        resolve(true);
+      })
+    })
 
+  }
 }
